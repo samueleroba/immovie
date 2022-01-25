c13923d0f928d545f8dcd8018d15dd342b2c9071
samueleroba <95041024+samueleroba@users.noreply.github.com>
2022-01-25 22:38:08 +0100

    Add files via upload

---
+++ src/api.php
@@ -0,0 +1,49 @@
+
+<?php
+    $title = htmlspecialchars($_GET["title"]);
+    $page = htmlspecialchars($_GET["page"]);
+    $type = htmlspecialchars($_GET["type"]);
+    $apikey = "b13dc6e8";
+
+
+
+
+echo CallAPI("GET","https://www.omdbapi.com/?apikey="+$apikey+"&plot=full&s="+$title+"&page="+$page+"&type="+$type);
+
+
+
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method)
+    {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+
+    // Optional Authentication:
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+
+    curl_close($curl);
+
+    return $result;
+}
+?>
